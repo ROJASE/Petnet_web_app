a59bc209614ba0da6a10680914bab5ff9a230f1b
ROJASE <42590681+ROJASE@users.noreply.github.com>
2021-04-22 14:56:44 -0500

    Update File Paths

--- start_sample_manager.sh
+++ start_sample_manager.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/petnet
 pwd
-node sample_manager.js --topic system_status --root-ca ~/BeagleBone_AWS_Cert/root-ca.pem --cert ~/BeagleBone_AWS_Cert/beaglebone-certificate.pem.crt --key ~/BeagleBone_AWS_Cert/beaglebon-private.pem.key --endpoint a17wfxz40xwmf5-ats.iot.us-west-2.amazonaws.com
+node sample_manager.js --topic system_status --root-ca ~/petnet/Petnet_web_app/BeagleBone_AWS_Cert/root-ca.pem --cert ~/petnet/Petnet_web_app/BeagleBone_AWS_Cert/beaglebone-certificate.pem.crt --key ~/petnet/Petnet_web_app/BeagleBone_AWS_Cert/beaglebon-private.pem.key --endpoint a17wfxz40xwmf5-ats.iot.us-west-2.amazonaws.com
