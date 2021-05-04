e017584ce80890b8c43a32e713f398e759b1cd51
ROJASE <42590681+ROJASE@users.noreply.github.com>
2021-05-04 14:25:22 -0500

    update

--- start_sample_manager.sh
+++ start_sample_manager.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ~/petnet
+cd ~/Petnet_web_app
 pwd
 node sample_manager.js --topic test/testing --root-ca ~/Petnet_web_app/BeagleBone_AWS_Cert/root-ca.pem --cert ~/Petnet_web_app/BeagleBone_AWS_Cert/beaglebone-certificate.pem.crt --key ~/Petnet_web_app/BeagleBone_AWS_Cert/beaglebon-private.pem.key --endpoint a17wfxz40xwmf5-ats.iot.us-west-2.amazonaws.com
